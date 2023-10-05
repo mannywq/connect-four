0db3de029f5694ea2a450b9a1c5c00ba8a259aa5
Manny <manny.wetterqvist@outlook.com>
2023-10-05 21:21:08 +0900

    working on calculating connected rows

--- lib/board.rb
+++ lib/board.rb
@@ -23,19 +23,76 @@ class Board
     false
   end
 
-  def place_marker(marker, col)
-    if open?(col)
-
-      (@grid.length - 1).downto(0) do |row|
-        if @grid[row][col - 1] == empty_circle
-          @grid[row][col - 1] = marker
-          # puts "Marker placed at grid #{grid[row][col]}"
-        end
+  def full?
+    @grid.each_with_index do |row, ri|
+      row.each_with_index do |_col, ci|
+        return false if @grid[ri][ci] == empty_circle
       end
-    else
-
-      puts 'No open slot'
-
     end
+    true
+  end
+
+  def place_marker(marker, col)
+    return false unless open?(col)
+
+    (@grid.length - 1).downto(0) do |row|
+      next unless @grid[row][col - 1] == empty_circle
+
+      @grid[row][col - 1] = marker
+      return true
+      # puts "Marker placed at grid #{grid[row][col]}"
+    end
+  end
+
+  def check_next?(pos, direction = 'x+', marker)
+    y, x = pos
+
+    return false if pos.any?(&:negative?)
+
+    case direction
+    when 'x+'
+      x + 1 <= @grid[y].length && @grid[y][x + 1] == marker
+    when 'y+'
+
+      puts "Checking for #{marker}"
+      y + 1 <= @grid.length - 1 && @grid[y + 1][x] == marker
+    when 'y-'
+      puts "Checking for #{marker}"
+      (y - 1).negative? == false && @grid[y - 1][x] == marker
+    when 'x-'
+      (x - 1).negative? == false && @grid[y][x - 1] == marker
+    end
+  end
+
+  def vert_row(col, marker)
+    return false if col < 1 || col > @grid.length - 1
+
+    length = 0
+    max = 0
+
+    (@grid.length - 1).downto(0) do |row|
+      length += 1 if @grid[row][col - 1] == marker
+      max = length if length > max
+      length = 0 if @grid[row][col - 1] != marker
+      return 4 if length == 4
+    end
+    [length, max].max
+  end
+
+  def hor_row(marker)
+    length = 0
+    max = 0
+
+    (@grid.length - 1).downto(0) do |row|
+      (0..(@grid[row].length - 1)).each do |col|
+        length += 1 if @grid[row][col] == marker
+        max = length if length > max
+        length = 0 if @grid[row][col] != marker
+        return 4 if length == 4
+      end
+    end
+    p length
+    p max
+    [length, max].max
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -21,27 +21,31 @@ describe Board do
   describe '#place_marker' do
     subject(:board) { described_class.new }
     before do
-      allow(board).to receive(:open?).and_return(true, false)
+      allow(board).to receive(:open?).and_return(true)
     end
     it 'accepts a marker' do
       grid = board.grid
       grid[0][0] = 'X'
       board.place_marker('X', 1)
 
-      expect(grid[1][0]).to eq('X')
+      expect(grid[5][0]).to eq('X')
     end
-    it 'prints an error message when the column is full' do
-      $stdout = StringIO.new
-      output = $stdout
-
-      board.place_marker('X', 1)
-
-      expect(output.string).to include('No open slot')
+    it 'returns false when the column is full' do
+      allow(board).to receive(:open?).and_return(false)
+      expect(board.place_marker('X', 1)).to be false
     end
   end
 
   describe '#full?' do
-    xit 'Returns true when all slots are full' do
+    it 'Returns true when all slots are full' do
+      grid = board.grid
+      grid.each_with_index do |row, ri|
+        row.each_with_index do |_col, ci|
+          grid[ri][ci] = board.yellow_circle
+        end
+      end
+      board.print_board
+      expect(board.full?).to be true
     end
   end
 
@@ -67,5 +71,87 @@ describe Board do
   describe '#has_winner?(player)' do
     xit 'it returns true when player has secured a winning combination' do
     end
+  end
+
+  describe '#check_next?([y, x], direction)' do
+    it 'Checks for the same marker vertically with y+' do
+      board.grid[0][0] = 'X'
+      board.grid[1][0] = 'X'
+      expect(board.check_next?([0, 0], 'y+', 'X')).to be true
+    end
+    it 'Checks for the same marker vertically with y-' do
+      board.grid[0][0] = 'X'
+      board.grid[1][0] = 'X'
+      expect(board.check_next?([1, 0], 'y-', 'X')).to be true
+    end
+
+    it 'Checks for the same marker horizontally with x+' do
+      board.grid[0][0] = 'X'
+      board.grid[0][1] = 'X'
+      expect(board.check_next?([0, 0], 'x+', 'X')).to be true
+    end
+    it 'Checks for the same marker horizontally with x-' do
+      board.grid[0][0] = 'X'
+      board.grid[0][1] = 'X'
+      expect(board.check_next?([0, 1], 'x-', 'X')).to be true
+    end
+  end
+  describe '#vert_row' do
+    let(:row_board) { described_class.new }
+    it 'Should return 1 when only 1 marker exists in column' do
+      row_board.place_marker('X', 1)
+
+      row_board.print_board
+      marker = 'X'
+      expect(row_board.vert_row(1, marker)).to eq 1
+    end
+    it 'Should return the number of markers in a row when more than 1' do
+      marker = 'X'
+
+      2.times { row_board.place_marker('X', 1) }
+      row_board.print_board
+
+      expect(row_board.vert_row(1, marker)).to eq 2
+    end
+    it 'Should return 4 when 4 or more markers are in a row' do
+      marker = 'X'
+
+      5.times { row_board.place_marker(marker, 1) }
+      row_board.print_board
+      expect(row_board.vert_row(1, marker)).to eq 4
+    end
+    it 'should return max consecutive values when values are mixed' do
+      marker1 = 'X'
+      marker2 = 'O'
+
+      row_board.place_marker(marker1, 1)
+      2.times { row_board.place_marker(marker2, 1) }
+      3.times { row_board.place_marker(marker1, 1) }
+
+      row_board.print_board
+
+      expect(row_board.vert_row(1, marker1)).to eq 3
+    end
+  end
+
+  describe '#hor_row' do
+    let(:row_board) { described_class.new }
+
+    it 'should return 1 when 1 marker is found in column' do
+      marker = 'X'
+      row_board.place_marker(marker, 1)
+      expect(row_board.hor_row(marker)).to eq 1
+    end
+
+    it 'should return the number of markers in a row if more than 1' do
+      marker = 'X'
+      3.times { |num| row_board.place_marker(marker, num + 1) }
+      row_board.print_board
+
+      expect(row_board.hor_row(marker)).to eq 3
+    end
+  end
+
+  describe 'diag_row' do
   end
 end
